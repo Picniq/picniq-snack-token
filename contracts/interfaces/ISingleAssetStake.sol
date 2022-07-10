87a2ca79fb44f37b5d8345528d873dcc40206032
dperk6 <daniel@perklin.ca>
2022-07-10 11:38:00 -0400

    stake interface

--- contracts/interfaces/ISingleAssetStake.sol
+++ contracts/interfaces/ISingleAssetStake.sol
@@ -9,6 +9,7 @@ interface ISingleAssetStake {
     function balanceOf(address account) external view returns (uint256);
     function rewardPerToken() external view returns (uint256);
     function earned(address account) external view returns (uint256);
+    function stake(uint256 amount) external payable;
     function withdraw(uint256 amount) external payable;
     function getReward() external;
     function exit() external;
