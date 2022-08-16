ebbed1e3731bb89a8fe221936204c1498fc7f0eb
dperk6 <daniel@perklin.ca>
2022-08-15 21:19:22 -0400

    reorg

--- contracts/PicniqToken.sol
+++ contracts/PicniqToken.sol
@@ -2,125 +2,28 @@
 
 pragma solidity 0.8.15;
 
+import "./PicniqVesting.sol";
 import "./PicniqTokenClaim.sol";
 import "./utils/ERC20Permit.sol";
 
 // solhint-disable no-inline-assembly, not-rely-on-time 
-contract PicniqToken is PicniqTokenClaim, ERC20Permit {
+contract PicniqToken is ERC20Permit {
 
-    mapping (address => UserVest) private _vesting;
-    uint256 private _totalVested;
-    ClaimDecay private _decayData;
-    
-    struct ClaimDecay {
-        uint64 decayTime;
-        uint64 decayRate;
-    }
+    PicniqVesting public vesting;
+    PicniqTokenClaim public claim;
 
-    struct UserVest {
-        uint8 length;
-        uint64 endTime;
-        uint256 amount;
-        uint256 withdrawn;
-    }
-
-    constructor(uint256 initialSupply, address treasury, address team, bytes32 merkleRoot_)
-        PicniqTokenClaim(merkleRoot_)
+    constructor(uint256 supply, address treasury, address team, bytes32 merkleRoot_)
         ERC20("Picniq Finance", "SNACK")
     {
-        uint256 teamAmount = initialSupply * 15 / 100;
-        uint256 treasuryAmount = initialSupply * 35 / 100;
+        uint256 teamAmount = supply * 15 / 100;
+        uint256 airdropAmount = 463364 * 10 * 10**18;
+        uint256 treasuryAmount = supply - teamAmount - airdropAmount;
+
+        vesting = new PicniqVesting(IERC20(this), _msgSender());
+        claim = new PicniqTokenClaim(IERC20(this), address(vesting), merkleRoot_, treasury);
 
         _mint(team, teamAmount);
         _mint(treasury, treasuryAmount);
-        _mint(address(this), initialSupply - treasuryAmount - teamAmount);
-
-        _decayData.decayTime = uint64(block.timestamp + (2628000 * 6));
-        _decayData.decayRate = 5e15;
-    }
-
-    function totalVested() external view returns (uint256)
-    {
-        return _totalVested;
-    }
-
-    function vestedOfDetails(address account) external view returns (UserVest memory)
-    {
-        return _vesting[account];
-    }
-
-    function vestedOf(address account) public view returns (uint256)
-    {
-        UserVest memory userVest = _vesting[account];
-
-        if (userVest.endTime <= block.timestamp) {
-            return userVest.amount - userVest.withdrawn;    
-        } else {
-            uint256 percent = block.timestamp * 1e18 / userVest.endTime;
-            return userVest.amount - (userVest.amount * percent / 1e18) - userVest.withdrawn;
-        }
-    }
-
-    function unvest() external
-    {
-        uint256 vested = vestedOf(msg.sender);
-        require(vested > 0, "No tokens to unvest");
-        _vesting[msg.sender].withdrawn += vested;
-        _totalVested -= vested;
-        transfer(msg.sender, vested);
-    }
-
-    function claimTokens(bytes32[] calldata proof, uint256 amount) external
-    {
-        require(checkProof(msg.sender, proof, amount), "Proof failed");
-
-        uint256 decay = 1e18;
-        if (block.timestamp > _decayData.decayTime) {
-            uint256 delta = block.timestamp - _decayData.decayTime;
-            if (delta > 86400) {
-                uint256 newRate = (delta / 86400) * 1e18 / _decayData.decayRate;
-                if (newRate > (1e18 / 2)) {
-                    decay = newRate;
-                } else {
-                    decay = 1e18 / 2;
-                }
-            }
-        }
-
-        transfer(msg.sender, amount * 10 * decay / 1e18);
-    }
-
-    function claimAndVest(bytes32[] calldata proof, uint256 amount, uint8 length) external
-    {
-        require(length == 6 || length == 12, "Length must be 6 or 12 months");
-        require(checkProof(msg.sender, proof, amount), "Proof failed");
-
-        uint256 decay = 1e18;
-        if (block.timestamp > _decayData.decayTime) {
-            uint256 delta = block.timestamp - _decayData.decayTime;
-            if (delta > 86400) {
-                uint256 newRate = (delta / 86400) * 1e18 / _decayData.decayRate;
-                if (newRate > (1e18 / 2)) {
-                    decay = newRate;
-                } else {
-                    decay = 1e18 / 2;
-                }
-            }
-        }
-
-        uint256 bonus = length == 6 ? 1.10e18 : 1.25e18;
-        uint256 total = (amount * 10 * decay / 1e18) * bonus / 1e18;
-        uint256 vested = total / 2;
-
-        _vesting[msg.sender] = UserVest({
-            length: length,
-            endTime: uint64(block.timestamp) + (2628000 * length),
-            amount: vested,
-            withdrawn: 0
-        });
-
-        _totalVested += vested;
-
-        transfer(msg.sender, total - vested);
+        _mint(address(claim), airdropAmount);
     }
 }
--- contracts/PicniqTokenClaim.sol
+++ contracts/PicniqTokenClaim.sol
@@ -2,10 +2,21 @@
 
 pragma solidity 0.8.15;
 
+import "./interfaces/IPicniqVesting.sol";
 import "./libraries/MerkleProof.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-abstract contract PicniqTokenClaim {
+contract PicniqTokenClaim {
     using MerkleProof for bytes32[];
+
+    ClaimDecay private _decayData;
+
+    uint256 public leftover;
+    uint256 public end;
+    address private _treasury;
+
+    IERC20 public immutable SNACK;
+    IPicniqVesting public immutable VESTING;
 
     bytes32 internal _merkleRoot;
     mapping(address => bool) internal _claims;
@@ -15,8 +26,74 @@ abstract contract PicniqTokenClaim {
         uint256 amount;
     }
 
-    constructor(bytes32 merkleRoot_) {
+    struct ClaimDecay {
+        uint64 decayTime;
+        uint64 decayRate;
+    }
+
+    constructor(IERC20 token, address vesting, bytes32 merkleRoot_, address treasury) {
+        SNACK = token;
+        VESTING = IPicniqVesting(vesting);
+
         _merkleRoot = merkleRoot_;
+        _treasury = treasury;
+        _decayData.decayTime = uint64(block.timestamp + (2628000 * 6));
+        _decayData.decayRate = 5e15;
+
+        end = block.timestamp + (2628000 * 12);
+    }
+
+    function claimTokens(bytes32[] calldata proof, uint256 amount) external
+    {
+        require(checkProof(msg.sender, proof, amount), "Proof failed");
+        require(block.timestamp < end, "Claiming is over");
+
+        uint256 decay = currentDecayPercent();
+        uint256 max = amount * 10 * 1.25e18 / 1e18;
+        uint256 sendAmount = amount * 10 * decay / 1e18;
+
+        leftover += max - sendAmount;
+
+        SNACK.transfer(msg.sender, sendAmount);
+    }
+
+    function claimAndVest(bytes32[] calldata proof, uint256 amount, uint8 length) external
+    {
+        require(length == 6 || length == 12, "Length must be 6 or 12 months");
+        require(checkProof(msg.sender, proof, amount), "Proof failed");
+        require(block.timestamp < end, "Claiming is over");
+
+        uint256 decay = currentDecayPercent();
+
+        uint256 bonus = length == 6 ? 1.1e18 : 1.25e18;
+        uint256 max = (amount * 10) * 1.25e18 / 1e18;
+        uint256 total = (amount * 10 * decay / 1e18) * bonus / 1e18;
+        uint256 vested = total / 2;
+
+        leftover += max - total;
+
+        SNACK.approve(address(VESTING), vested);
+        VESTING.vestTokens(msg.sender, vested, length);
+        SNACK.transfer(msg.sender, total - vested);
+    }
+
+    function currentDecayPercent() public view returns (uint256)
+    {
+        uint256 decay = 1e18;
+
+        if (block.timestamp > _decayData.decayTime) {
+            uint256 delta = block.timestamp - _decayData.decayTime;
+            if (delta > 86400) {
+                uint256 newRate = (delta / 86400) * 1e18 / _decayData.decayRate;
+                if (newRate > (1e18 / 2)) {
+                    decay = newRate;
+                } else {
+                    decay = 1e18 / 2;
+                }
+            }
+        }
+
+        return decay;
     }
 
     function checkClaimed(address account) public view returns (bool)
@@ -24,7 +101,7 @@ abstract contract PicniqTokenClaim {
         return _claims[account];
     }
 
-    function checkProof(address account, bytes32[] calldata proof, uint256 amount) internal returns (bool)
+    function checkProof(address account, bytes32[] calldata proof, uint256 amount) public returns (bool)
     {
         require(!checkClaimed(account), "Already claimed");
 
@@ -34,5 +111,20 @@ abstract contract PicniqTokenClaim {
         _claims[account] = true;
         
         return true;
+    }
+
+    function withdrawLeftover() external
+    {
+        require(msg.sender == _treasury, "Only treasury address can withdraw");
+
+        if (block.timestamp > end) {
+            uint256 balance = SNACK.balanceOf(address(this));
+            SNACK.transfer(_treasury, balance);
+            leftover = 0;
+        } else {
+            uint256 sendAmount = leftover;
+            leftover = 0;
+            SNACK.transfer(_treasury, sendAmount);
+        }
     }
 }
